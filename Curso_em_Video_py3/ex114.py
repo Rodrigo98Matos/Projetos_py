84a7ba9cf93be77ce000e2d1651dd09e9508c3aa
rodrigo98matos <rodrigo98matos@gmail.com>
2021-05-12 09:15:51 -0300

    Update ex114.py

--- Curso_em_Video_py3/ex114.py
+++ Curso_em_Video_py3/ex114.py
@@ -1,2 +1,8 @@
 from utilidadesCeV import web
-print(web.valida_url('http://pudim.com.br/',True))
+# print(web.valida_url('http://pudim.com.br/',True))
+try:
+    a = open('texte.txt','w+')
+    b = (web.valida_url('file:///C:/Users/alber/Documents/MeusProjetos/Ola_Mundo/site_exemplo/index.html',True))
+    a.write(f'{b}')
+finally:
+    a.close()
